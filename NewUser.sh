a4a7b60c9b43cbba75f90f6d2999be60fc93f506
Maze <matthias.binner41@googlemail.com>
2018-10-16 12:22:55 +0200

    Add files via upload

--- NewUser.sh
+++ NewUser.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-read -p "Please input the desired username and press [ENTER]:  " username
-
+echo "$(tput bold)###############################################################$(tput sgr0)"
+echo "$(tput bold)##    Please input the desired username and press [ENTER]    ##$(tput sgr0)"
+echo "$(tput bold)###############################################################$(tput sgr0)"
+read -p "Username: " username
+echo ""
 # FOR UBUNTU sudo su -c "useradd $username -s /bin/bash -m -G adm, cdrom, sudo, dip, plugdev, lpadmin, sambashare"
 sudo su -c "useradd $username -s /bin/bash -m -G adm,cdrom,sudo,dip,plugdev" # FOR DEBIAN
-echo -e "Please input a new password for the USER account:  "
+echo "$(tput bold)################################################################$(tput sgr0)"
+echo "$(tput bold)##      Please input a new password for the USER account      ##$(tput sgr0)"
 sleep 1
 sudo passwd $username
-echo -e "Please input a new password for the ROOT access: "
+echo "$(tput bold)################################################################$(tput sgr0)"
+echo ""
+
+echo "$(tput bold)################################################################$(tput sgr0)"
+echo "$(tput bold)##      Please input a new password for the ROOT account      ##$(tput sgr0)"
 sleep 1
-sudo passwd
+sudo passwd 
+echo "$(tput bold)################################################################$(tput sgr0)"
+echo ""
+
+echo "$(tput bold)################################################################$(tput sgr0)"
+sleep 1
 echo -e "All done. Please log out and log in again and choose the new profile."
